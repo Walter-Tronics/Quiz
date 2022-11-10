2b225204ddeabe0aa27a9820160d87280e78edaf
Walter Tronics <waltertronics02@gmail.com>
2022-11-10 16:13:44 +0100

    Timer functionality
    Animating the timer and setting event listeners for buttons and also setting mark functionality to option boxes

--- Quiz.js
+++ Quiz.js
@@ -6,6 +6,8 @@ var answer3 = document.getElementById('option(3)1')
 var answer4 = document.getElementById('option(4)3');
 function retry(){
   document.getElementById("pop2").style.display="none";
+  clearInterval(interval);
+  countdown();
 }
 var myindex=0;
 next();
@@ -18,7 +20,23 @@ function next(){
   myindex++;
   if(myindex>arr.length){myindex=1}
   arr[myindex-1].style.display="block";
+  prog_value = 100;
+  clearInterval(interval)
+  countdown();
 }
+
+//Animating the timer for countdown
+var prog_value = 100,
+interval;
+let timer = document.querySelector('.timerVal');
+function countdown(){
+  interval = setInterval(()=>{
+  prog_value-=.5;
+  timer.style.width = `${prog_value}%`
+  }, 50);
+}
+
+
 var correct = document.querySelector('#correct');
 var wrong = document.querySelector('#wrong');
 var done = document.querySelector('#done');
@@ -26,39 +44,59 @@ var pop=document.getElementById("pop");
 var pop2=document.getElementById("pop2");
 var pop3=document.getElementById("pop3");
 
-function check1(){
+
+//Clicking on box marks the radio input tag
+let optBox = document.querySelectorAll(".opt");
+//looping through the boxes
+for (let i of optBox) {
+  //add an eventlistener to each of the boxes 
+  i.addEventListener('click', ()=>{
+    i.querySelector('input').checked = true;
+  });
+}
+
+
+//Checking for the correct answer functions
+document.getElementById("check1").addEventListener('click', ()=>{
   if (answer1.checked) {
-    correct.play();
-    pop.style.display="block";
-  }else{
+      clearInterval(interval);
+      correct.play();
+      pop.style.display="block";
+  }else {
     wrong.play();
     pop2.style.display="block";
   }
-}
-function check2(){
+});
+
+document.getElementById("check2").addEventListener('click', ()=>{
   if (answer2.checked) {
-    correct.play();
-    pop.style.display="block";
+      clearInterval(interval);
+      correct.play();
+      pop.style.display="block";
   }else{
     wrong.play();
     pop2.style.display="block";
   }
-}
-function check3(){
+});
+
+document.getElementById("check3").addEventListener('click', ()=>{
   if (answer3.checked) {
-    correct.play();
-    pop.style.display="block";
+      clearInterval(interval);
+      correct.play();
+      pop.style.display="block";
   }else{
     wrong.play();
     pop2.style.display="block";
   }
-}
-function check4(){
+});
+
+document.getElementById("check4").addEventListener('click', ()=>{
   if (answer4.checked) {
-    done.play();
-    pop3.style.display="block";
+      clearInterval(interval);
+      done.play();
+      pop3.style.display="block";
   }else{
     wrong.play();
     pop2.style.display="block";
   }
-}
+});
